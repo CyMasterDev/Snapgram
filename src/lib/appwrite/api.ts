af3172a04de8c60b173535d2ea9d5d7c53549308
edward <eanutt1272@gmail.com>
2025-01-05 14:36:12 +0000

    toast, reactquery, api change, functions

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,12 +1,18 @@
 import { ID } from 'appwrite';
 
 import { INewUser } from "@/types";
+import { account } from './config';
 
 export async function createUserAccount(user: INewUserwUser) {
   try {
     const newAccount = await account.create(
         ID.unique(),
+        user.email,
+        user.password,
+        user.name,
     )
+
+    return newAccount
   } catch (error) {
     console.log(error);
     return error;
