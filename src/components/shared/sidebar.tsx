19d77e5200ce83e0d4c32c678d5fafa082f9d6f2
CyMasterDev <CyMasterDev@outlook.com>
2025-01-18 19:29:52 +0000

    UI, sidebar & topbar

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react';
+import { Button } from '../ui/button'
+import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
+import { useUserContext } from '@/context/AuthContext';
 
 const Sidebar = () => {
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess])
+
   return (
-    <div>sidebar</div>
+    <nav className='leftsidebar'>
+      <div className='flex flex-col gap-11'>
+        <Link to='/' className='flex gap-3 items-center'>
+          <img
+            src='/assets/images/logo.svg'
+            alt='logo'
+            draggable="false"
+            className="select-none"
+            width={170}
+            height={36}
+          />
+        </Link>
+
+        <Link to={`/profile/${user.id}`} className='flex gap-3 items-center select-none'  draggable="false">
+          <img
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            draggable="false"
+            className="select-none"
+          />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
--- src/components/shared/topbar.tsx
+++ src/components/shared/topbar.tsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react';
-
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
 import { useUserContext } from '@/context/AuthContext';
@@ -8,7 +7,7 @@ import { useUserContext } from '@/context/AuthContext';
 const Topbar = () => {
   const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
-  const user = useUserContext();
+  const { user } = useUserContext();
 
   useEffect(() => {
     if (isSuccess) navigate(0);
@@ -19,27 +18,33 @@ const Topbar = () => {
       <div className='flex-between py-4 px-5'>
         <Link to='/' className='flex gap-3 items-center'>
           <img
-          src='/assets/images/logo.svg'
-          alt='logo'
-          draggable="false"
-          className="select-none"
-          width={130}
-          height={325}
+            src='/assets/images/logo.svg'
+            alt='logo'
+            draggable="false"
+            className="select-none"
+            width={130}
+            height={325}
           />
         </Link>
 
         <div className='flex gap-4'>
-          <Button variant='ghost' className='shad-button_ghost' onClick={() => signOut()}>
+            <Button variant='ghost' className='shad-button_ghost' onClick={() => signOut()}>
               <img
-              src='/assets/icons/logout.svg'
-              alt='logout'
-              draggable="false"
-              className="select-none"
+                src='/assets/icons/logout.svg'
+                alt='logout'
+                draggable="false"
+                className="select-none"
               />
-          </Button>
+            </Button>
+          <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
+            <img
+              src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
+              alt='profile'
+              draggable="false"
+              className="h-8 w-8 rounded-full select-none"
+            />
+          </Link>
         </div>
-        <Link to={`/profile`}>
-        </Link>
       </div>
     </section>
   )
