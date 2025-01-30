e11b44a669cbae42dd5b040743c7c31b567e8d39
CyMasterDev <CyMasterDev@outlook.com>
2025-01-30 19:41:45 +0000

    add sidebar link constants and add styled sidebar links

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,8 +1,10 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react';
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
 import { useUserContext } from '@/context/AuthContext';
+import { sidebarLinks } from '@/constants';
+import { INavLink } from '@/types';
 
 const Sidebar = () => {
   const { mutate: signOut, isSuccess } = useSignOutAccount();
@@ -27,13 +29,46 @@ const Sidebar = () => {
           />
         </Link>
 
-        <Link to={`/profile/${user.id}`} className='flex gap-3 items-center select-none'  draggable="false">
+        <Link to={`/profile/${user.id}`} className='flex gap-3 items-center select-none' draggable="false">
           <img
             src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt='profile'
             draggable="false"
-            className="select-none"
+            className="w-14 h-14 rounded-full select-none"
           />
+
+          <div className='flex flex-col'>
+            <p className='body-bold'>
+              {user.name}
+            </p>
+
+            <p className='small-regular text-light-3'>
+              @{user.username}
+            </p>
+          </div>
         </Link>
+
+        <ul className='flex flex-col gap-6'>
+          {sidebarLinks.map((link: INavLink) => {
+            return (
+              <li key={link.label}
+              className='leftsidebar-link'>
+                <NavLink
+                  to={link.route}
+                  className='flex gap-4 items-center p-4'
+                >
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    draggable="false"
+                    className='group-hover:invert-white select-none'
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </nav>
   )
